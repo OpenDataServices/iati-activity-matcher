8f750f8afc616553fe0fd953d17cedaeee8cb454
Ben Webb <ben.webb@opendataservices.coop>
2022-06-22 17:14:00 +0100

    Also match transaction values

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Activity Matching demo using CGIAR and BMGF data
+
+Only works where both publishers use USD.
--- match.py
+++ match.py
@@ -5,15 +5,25 @@ import sys
 from fuzzywuzzy import fuzz
 from lxml import etree
 
+# Load CGIAR activities
 tree = etree.parse("xml_in/cgiar-activities.xml")
+# Use only those with BMGF as a participating org
 cgiar_activities = tree.xpath("/iati-activities/iati-activity[participating-org[@ref='DAC-1601']]")
 
+# Load BMGF activities
 bmgf_activities = []
+# Filter to only these Channel Codes, which are specifically for CGIAR and it's members
 crs_channel_codes = [47015, 47017, 47018, 47020, 47021, 47051, 47056, 47057, 51001, 47062, 47063, 47069, 47070, 47075, 47101, 47104]
 crs_channel_codes_or_statement = " or ".join(f"@crs-channel-code='{code}'" for code in crs_channel_codes)
 for bmgf_file in glob.glob("xml_in/bmgf-activity-*.xml"):
     tree = etree.parse(bmgf_file)
     bmgf_activities += tree.xpath(f"/iati-activities/iati-activity[participating-org[{crs_channel_codes_or_statement}]]")
+
+def activity_lookup(activities):
+    return {activity.xpath("iati-identifier")[0].text: activity for activity in activities}
+
+cgiar_activity_lookup = activity_lookup(cgiar_activities)
+bmgf_activity_lookup = activity_lookup(bmgf_activities)
 
 def titles(activities):
     return {activity.xpath("iati-identifier")[0].text: "\n".join(activity.xpath("title/narrative/text()")) for activity in activities}
@@ -30,4 +40,19 @@ for cgiar_iati_identifier, cgiar_title in cgiar_titles.items():
         ratios.append((ratio, bmgf_iati_identifier, bmgf_title))
     ratios.sort(reverse=True)
     top_match = ratios[0]
-    csvwriter.writerow([top_match[0], cgiar_iati_identifier, top_match[1], cgiar_title, top_match[2]])
+
+    transactions = cgiar_activity_lookup[cgiar_iati_identifier].findall("transaction")
+    transactions = [transaction for transaction in transactions if transaction.xpath("transaction-type/@code")[0] != "2"]
+    # Assume everything's USD
+    cgiar_values = [value.text for value in [transaction.find("value") for transaction in transactions]]
+
+    assert len(cgiar_values) == 1
+
+    bmgf_iati_identifier = top_match[1]
+    bmgf_title = top_match[2]
+    transactions = bmgf_activity_lookup[bmgf_iati_identifier].findall("transaction")
+    bmgf_values = [value.text for value in [transaction.find("value") for transaction in transactions]]
+
+    transaction_match = cgiar_values[0] in bmgf_values
+
+    csvwriter.writerow([top_match[0], cgiar_iati_identifier, top_match[1], cgiar_title, top_match[2], transaction_match])

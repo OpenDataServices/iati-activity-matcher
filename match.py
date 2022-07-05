4a2369a36057b1087ec4a1d43c5bde088587ffc5
Ben Webb <ben.webb@opendataservices.coop>
2022-07-05 12:53:44 +0100

    Add linting, reformat with black

--- match.py
+++ match.py
@@ -8,25 +8,59 @@ from lxml import etree
 # Load CGIAR activities
 cgiar_tree = etree.parse("xml_in/cgiar-activities.xml")
 # Use only those with BMGF as a participating org
-cgiar_activities = cgiar_tree.xpath("/iati-activities/iati-activity[participating-org[@ref='DAC-1601']]")
+cgiar_activities = cgiar_tree.xpath(
+    "/iati-activities/iati-activity[participating-org[@ref='DAC-1601']]"
+)
 
 # Load BMGF activities
 bmgf_activities = []
 # Filter to only these Channel Codes, which are specifically for CGIAR and it's members
-crs_channel_codes = [47015, 47017, 47018, 47020, 47021, 47051, 47056, 47057, 51001, 47062, 47063, 47069, 47070, 47075, 47101, 47104]
-crs_channel_codes_or_statement = " or ".join(f"@crs-channel-code='{code}'" for code in crs_channel_codes)
+crs_channel_codes = [
+    47015,
+    47017,
+    47018,
+    47020,
+    47021,
+    47051,
+    47056,
+    47057,
+    51001,
+    47062,
+    47063,
+    47069,
+    47070,
+    47075,
+    47101,
+    47104,
+]
+crs_channel_codes_or_statement = " or ".join(
+    f"@crs-channel-code='{code}'" for code in crs_channel_codes
+)
 for bmgf_file in glob.glob("xml_in/bmgf-activity-*.xml"):
     tree = etree.parse(bmgf_file)
-    bmgf_activities += tree.xpath(f"/iati-activities/iati-activity[participating-org[{crs_channel_codes_or_statement}]]")
+    bmgf_activities += tree.xpath(
+        f"/iati-activities/iati-activity[participating-org[{crs_channel_codes_or_statement}]]"
+    )
+
 
 def activity_lookup(activities):
-    return {activity.xpath("iati-identifier")[0].text: activity for activity in activities}
+    return {
+        activity.xpath("iati-identifier")[0].text: activity for activity in activities
+    }
+
 
 cgiar_activity_lookup = activity_lookup(cgiar_activities)
 bmgf_activity_lookup = activity_lookup(bmgf_activities)
 
+
 def titles(activities):
-    return {activity.xpath("iati-identifier")[0].text: "\n".join(activity.xpath("title/narrative/text()")) for activity in activities}
+    return {
+        activity.xpath("iati-identifier")[0].text: "\n".join(
+            activity.xpath("title/narrative/text()")
+        )
+        for activity in activities
+    }
+
 
 cgiar_titles = titles(cgiar_activities)
 bmgf_titles = titles(bmgf_activities)
@@ -45,25 +79,52 @@ for cgiar_iati_identifier, cgiar_title in cgiar_titles.items():
     ratio, bmgf_iati_identifier, bmgf_title = top_match
 
     if ratio >= 90:
-        participating_org = cgiar_activity_lookup[cgiar_iati_identifier].find("participating-org[@ref='DAC-1601']")
+        participating_org = cgiar_activity_lookup[cgiar_iati_identifier].find(
+            "participating-org[@ref='DAC-1601']"
+        )
         participating_org.attrib["activity-id"] = bmgf_iati_identifier
 
-    cgiar_transactions = cgiar_activity_lookup[cgiar_iati_identifier].findall("transaction")
-    cgiar_transactions = [transaction for transaction in cgiar_transactions if transaction.xpath("transaction-type/@code")[0] != "2"]
+    cgiar_transactions = cgiar_activity_lookup[cgiar_iati_identifier].findall(
+        "transaction"
+    )
+    cgiar_transactions = [
+        transaction
+        for transaction in cgiar_transactions
+        if transaction.xpath("transaction-type/@code")[0] != "2"
+    ]
     # Assume everything's USD
-    cgiar_values = [value.text for value in [transaction.find("value") for transaction in cgiar_transactions]]
+    cgiar_values = [
+        value.text
+        for value in [transaction.find("value") for transaction in cgiar_transactions]
+    ]
 
     assert len(cgiar_values) == 1
 
-    bmgf_transactions = bmgf_activity_lookup[bmgf_iati_identifier].findall("transaction")
-    bmgf_values = [value.text for value in [transaction.find("value") for transaction in bmgf_transactions]]
+    bmgf_transactions = bmgf_activity_lookup[bmgf_iati_identifier].findall(
+        "transaction"
+    )
+    bmgf_values = [
+        value.text
+        for value in [transaction.find("value") for transaction in bmgf_transactions]
+    ]
 
     transaction_match = cgiar_values[0] in bmgf_values
 
-    csvwriter.writerow([ratio, cgiar_iati_identifier, bmgf_iati_identifier, cgiar_title, bmgf_title, transaction_match])
+    csvwriter.writerow(
+        [
+            ratio,
+            cgiar_iati_identifier,
+            bmgf_iati_identifier,
+            cgiar_title,
+            bmgf_title,
+            transaction_match,
+        ]
+    )
 
     if ratio >= 90 and transaction_match:
         print(cgiar_iati_identifier)
-        cgiar_transactions[0].find("provider-org").attrib["provider-activity-id"] = bmgf_iati_identifier
+        cgiar_transactions[0].find("provider-org").attrib[
+            "provider-activity-id"
+        ] = bmgf_iati_identifier
 
 cgiar_tree.write("out/cgiar-activities-matched.xml")
